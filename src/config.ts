2f425e499626dc36e4020348a44f1019a9bac9a0
Luke Swithenbank <lswith@users.noreply.github.com>
2019-07-16 17:38:04 +1000

    feat: do not share configuration between repositories

--- src/config.ts
+++ src/config.ts
@@ -3,25 +3,23 @@ import { Context } from "probot";
 
 export class ConfigManager<T> {
   private filename: string;
-  private config: T;
+  private defaultConfig: T;
   private schema: Joi.Schema;
 
   constructor(filename: string, defaultConfig: T, schema: Joi.Schema) {
     this.filename = filename;
-    this.config = defaultConfig;
+    this.defaultConfig = defaultConfig;
     this.schema = schema;
   }
 
   public async getConfig(context: Context): Promise<T> {
-    return context.config(this.filename, this.config).then(newConfig => {
+    return context.config(this.filename, this.defaultConfig).then(newConfig => {
       const result = this.validateConfig(newConfig);
       if (result.error) {
         const annotation = result.error.annotate();
         throw new Error(`Invalid Config: ${annotation}`);
-      } else {
-        this.config = newConfig;
       }
-      return this.config;
+      return newConfig;
     });
   }
 
